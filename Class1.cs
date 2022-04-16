c434120f4b41b61b95fdf74402761f04caefa8fa
victorcb00 <101061771+victorcb00@users.noreply.github.com>
2022-04-17 02:40:21 +0300

    Functia Radical / Afisare in consola

--- Class1.cs
+++ Class1.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace PS1B___Tema_10___Ciobanu_Victor
 {
-    internal class Class1
+    public class Class1
     {
+        public static void Radical(float numar, float eroare, ref float x1, ref int count)
+        {
+			float x0 = 0;
+			x1 = numar / 2;
+			count = 0;
+			do
+			{
+				x0 = x1;
+				x1 = (x0 + numar / x0) / 2;
+				count++;
+			} while (Math.Abs(x1 - x0) > eroare);
+		}
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,15 @@ namespace PS1B___Tema_10___Ciobanu_Victor
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            float numar = 1657;
+            float eroare = 0.001f;
+            float x1 = 0;
+            int count = 0;
+            Class1.Radical(numar, eroare, ref x1, ref count);
+            Console.WriteLine("{0} {1}", x1, count);
+        }
     }
 }
